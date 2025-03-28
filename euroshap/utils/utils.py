87e4b54d7c413f8a6ba8b8b6755677265e86a3c2
pdrew32 <patrickmdrew@gmail.com>
2025-03-27 21:03:27 -0400

    moved shape_of_cutout() to utils

--- euroshap/utils/utils.py
+++ euroshap/utils/utils.py
@@ -1,4 +1,5 @@
 import requests
+from torch.utils.data import Dataset
 
 
 def report_filesize(url):
@@ -6,3 +7,20 @@ def report_filesize(url):
     filesize = int(response.headers.get("Content-Length", 0))
     print("File size in MB:", filesize / (1024*1024))
     return
+
+
+def shape_of_cutout(dataset: Dataset) -> None:
+    """
+    Print the shape of the first image in the dataset
+
+    Args:
+        dataset (pytorch Dataset): The dataset to analyze. Each element 
+            is expected to be a tuple (image, label).
+
+    Returns:
+        None
+    """
+    img, _ = dataset[0]
+    shape = list(img.shape)
+    print(f'Shape of images [color, height, width]: {shape}')
+    return None
