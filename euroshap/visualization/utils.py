3f3c69d4a7790e447a36fb162c4e9ff69d784ad6
pdrew32 <patrickmdrew@gmail.com>
2025-03-27 21:00:02 -0400

    added shape_of_cutout

--- euroshap/visualization/utils.py
+++ euroshap/visualization/utils.py
@@ -26,3 +26,20 @@ def plot_cutout(dataset: Dataset, index: int) -> None:
     plt.title(f"Label: {dataset.classes[label]}")
     plt.axis('off')
     plt.show();
+
+
+def shape_of_cutout(dataset: Dataset) -> None:
+    """
+    Print the shape of the first image in the dataset
+
+    Args:
+        dataset (pytorch Dataset): The dataset to analyze. Each element 
+            is expected to be a tuple (image, label).
+
+    Returns:
+        None
+    """
+    img, _ = dataset[0]
+    shape = list(img.shape)
+    print(f'Shape of images [color, height, width]: {shape}')
+    return None
